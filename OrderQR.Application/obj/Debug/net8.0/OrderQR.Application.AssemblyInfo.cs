aff82b4d4fb2c405b1b7185558260183cd26f942
Bernardo Costa <bernardo.caetano94@gmail.com>
2024-04-12 09:19:33 -0300

    Alteração das cores primaria e secundária e começo do dashboard

--- OrderQR.Application/obj/Debug/net8.0/OrderQR.Application.AssemblyInfo.cs
+++ OrderQR.Application/obj/Debug/net8.0/OrderQR.Application.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderQR.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcea351eff9b7c871358de8180db24b43bb42ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderQR.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderQR.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
